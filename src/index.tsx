ef45edf93f9166eb586fccd75f6908ad0d38b6ae
Ikuma_Suwa <ecprogramming24@gmail.com>
2020-05-06 02:00:45 +0900

    コメント記述１

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "babel-polyfill";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import { render } from "react-dom";
+import Root from "./containers/Root";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<Root />, document.getElementById("root"));
+
+/* 
+Reddit：英語版の掲示板的なもの。2ch(5ch)的な？
+subreddit：redditでは特定の領域向けに焦点を当てたコミュニティを
+    サブレディット(subreddit)と呼ぶ。これは匿名掲示板2ちゃんねるなどにおける
+    『板』に相当するものであるが、ユーザーが自主的に立ち上げることが可能である。
+*/
